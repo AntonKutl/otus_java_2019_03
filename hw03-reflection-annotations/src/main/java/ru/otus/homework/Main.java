2a88717fa65b86ba7551d80abcebdee7e2876ead
Антон <write_me@live.ru>
2019-04-23 20:45:47 +0300

    исправил замечания

--- hw03-reflection-annotations/src/main/java/ru/otus/homework/Main.java
+++ hw03-reflection-annotations/src/main/java/ru/otus/homework/Main.java
@@ -5,5 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
         RunTest.start(UserTest.class);
+
+
     }
 }
