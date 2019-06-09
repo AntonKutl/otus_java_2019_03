fc7f67f5b57f6559dd4acb5a1054c47d0dd15fbd
Антон <write_me@live.ru>
2019-06-09 18:03:00 +0300

    8 задание

---
+++ hw-08/src/main/java/ru/otus/homework/examples/Person.java
@@ -0,0 +1,20 @@
+package ru.otus.homework.examples;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
