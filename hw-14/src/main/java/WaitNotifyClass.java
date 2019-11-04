81fb9f85f1a4a1b35bdc605516872ac565cff742
Anton Kutlubaev <write_me@live.ru>
2019-11-04 18:33:00 +0300

    объединены методы printeOne и printeTwo в метод printe

--- hw-14/src/main/java/WaitNotifyClass.java
+++ hw-14/src/main/java/WaitNotifyClass.java
@@ -6,84 +6,47 @@ public class WaitNotifyClass {
     public static void main(String[] args) {
         WaitNotifyClass w = new WaitNotifyClass();
         Thread oneThread = new Thread(() -> {
-            w.printOne();
+            w.print("one");
         });
         Thread twoThread = new Thread(() -> {
-            w.printTwo();
+            w.print("two");
         });
         oneThread.start();
         twoThread.start();
     }
 
-    public void printOne() {
+    public void print(String flag) {
         synchronized (monitor) {
-            StringBuffer str=new StringBuffer("Поток 1:");
+            StringBuffer str=new StringBuffer("Поток "+ flag+":");
             try {
+
                 for (int i = 1; i < 11; i++) {
-                    while (!flagTread.equals("one")) {
+                    Thread.sleep(350);
+                    while (!flagTread.equals(flag)) {
                         monitor.wait();
                     }
 
-                    System.out.print('\r');
-
-                    Thread.sleep(50);
-
                     str.append(i+" ");
-                    System.out.print(str);
-                    flagTread = "two";
+                    System.out.println(str);
+                    if (flag.equals("one"))flagTread = "two";
+                    else flagTread = "one";
                     monitor.notify();
                 }
                 for (int i = 9; i > 0; i--) {
-                    while (!flagTread.equals("one")) {
+
+                    Thread.sleep(350);
+
+                    while (!flagTread.equals(flag)) {
                         monitor.wait();
                     }
 
-                    System.out.print('\r');
-
-                    Thread.sleep(150);
-
                     str.append(i+" ");
-                    System.out.print(str);
-                    flagTread = "two";
+                    System.out.println(str);
+                    if (flag.equals("one"))flagTread = "two";
+                    else flagTread = "one";
                     monitor.notify();
                 }
 
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void printTwo() {
-
-        synchronized (monitor) {
-            StringBuffer str=new StringBuffer("Поток 2:");
-            try {
-                for (int i = 1; i < 11; i++) {
-                    while (!flagTread.equals("two")) {
-                        monitor.wait();
-                    }
-
-                    Thread.sleep(150);
-
-                    str.append(i+" ");
-                    System.out.print(str);
-                    flagTread= "one";
-                    monitor.notify();
-                }
-                for (int i = 9; i > 0; i--) {
-                    while (!flagTread.equals("two")) {
-                        monitor.wait();
-                    }
-
-                    Thread.sleep(100);
-
-                    str.append(i+" ");
-                    System.out.print(str);
-                    flagTread= "one";
-                    monitor.notify();
-                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
