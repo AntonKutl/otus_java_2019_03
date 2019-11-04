a8b94e6137909f8cfff149f8fa535953f9f8a966
Anton Kutlubaev <write_me@live.ru>
2019-11-04 19:53:07 +0300

    исправленны некоторые замечания

--- hw-14/src/main/java/WaitNotifyClass.java
+++ hw-14/src/main/java/WaitNotifyClass.java
@@ -1,7 +1,7 @@
 
 public class WaitNotifyClass {
     private final Object monitor = new Object();
-    private volatile String flagTread = "one";
+    private volatile String nameTread = "one";// блокировка потока по имени, с boolean чередование не получиться
 
     public static void main(String[] args) {
         WaitNotifyClass w = new WaitNotifyClass();
@@ -17,36 +17,20 @@ public class WaitNotifyClass {
 
     public void print(String flag) {
         synchronized (monitor) {
-            StringBuffer str=new StringBuffer("Поток "+ flag+":");
+            StringBuilder str=new StringBuilder("Поток "+ flag+":");
             try {
 
-                for (int i = 1; i < 11; i++) {
+                for (int i = 1; i < 19; i++) {
                     Thread.sleep(350);
-                    while (!flagTread.equals(flag)) {
+                    while (!nameTread.equals(flag)) {
                         monitor.wait();
                     }
 
-                    str.append(i+" ");
+                    str.append(i);
                     System.out.println(str);
-                    if (flag.equals("one"))flagTread = "two";
-                    else flagTread = "one";
+                    nameTread = nameTread.equals("one")?"two":"one";
                     monitor.notify();
                 }
-                for (int i = 9; i > 0; i--) {
-
-                    Thread.sleep(350);
-
-                    while (!flagTread.equals(flag)) {
-                        monitor.wait();
-                    }
-
-                    str.append(i+" ");
-                    System.out.println(str);
-                    if (flag.equals("one"))flagTread = "two";
-                    else flagTread = "one";
-                    monitor.notify();
-                }
-
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
